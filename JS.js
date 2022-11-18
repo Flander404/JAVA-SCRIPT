98e7b7cfd4690ca2d24cfc6886fc4642133e1146
Flander404 <109587664+Flander404@users.noreply.github.com>
2022-11-18 18:34:44 +0500

    BFVDZ
    B DFF

--- JS.js
+++ JS.js
@@ -1,61 +1,76 @@
-addEventListener('click', fn);
+// addEventListener('click', fn);
 
-setTimeout(() => {
-    removeEventListener('click', fn)
-},5000)
-let oldtar;
-function fn(ev) {
-    let tar = ev.target.closest('.all-img img');
-    if (!tar) return;
+// setTimeout(() => {
+//     removeEventListener('click', fn)
+// },5000)
+// let oldtar;
+// function fn(ev) {
+//     let tar = ev.target.closest('.all-img img');
+//     if (!tar) return;
 
-    oldtar ? oldtar.style.border = '' : '';
+//     oldtar ? oldtar.style.border = '' : '';
 
-    let src = tar.getAttribute('src');
+//     let src = tar.getAttribute('src');
 
-    document.querySelector('.show img').setAttribute('src', src);
+//     document.querySelector('.show img').setAttribute('src', src);
 
-    tar.style.border = '5px solid green';
-    oldtar = tar;
-}
-let text = document.querySelector('.text')
-text.onclick = function(e){
-    let olcham = text.getBoundingClientRect()
-    let textA;
-    console.log(olcham);
-    text.replaceWith(( function(){
-        textA = document.createElement('textarea')
-        textA.value = text.innerHTML;
-        textA.style.width = olcham.width +'px'
-        textA.style.height = olcham.height +'px'
-        return textA;
-    })())
-    let div;
-    textA.onkeydown = function(e){
-        if(e.ctrlKey && e.key == 'Enter'){
-            textA.replaceWith(( function(){
-                div = document.createElement('div')
-                div.innerHTML = textA.value;
-                div.style.textAlign = 'center'
-                div.style.border = '2px solid red'
-                return div;
-            })())   
-        }
+//     tar.style.border = '5px solid green';
+//     oldtar = tar;
+// }
+// let text = document.querySelector('.text')
+// text.onclick = function(e){
+//     let olcham = text.getBoundingClientRect()
+//     let textA;
+//     console.log(olcham);
+//     text.replaceWith(( function(){
+//         textA = document.createElement('textarea')
+//         textA.value = text.innerHTML;
+//         textA.style.width = olcham.width +'px'
+//         textA.style.height = olcham.height +'px'
+//         return textA;
+//     })())
+//     let div;
+//     textA.onkeydown = function(e){
+//         if(e.ctrlKey && e.key == 'Enter'){
+//             textA.replaceWith(( function(){
+//                 div = document.createElement('div')
+//                 div.innerHTML = textA.value;
+//                 div.style.textAlign = 'center'
+//                 div.style.border = '2px solid red'
+//                 return div;
+//             })())   
+//         }
+//     }
+
+// }
+
+// sayt yonaltirish
+
+// let inp = document.querySelector(".inp")
+
+// inp.addEventListener('keydown', function(e){
+//     if(!e.target.value) return;
+//     if(e.key == 'Enter'){
+//         let a = document.createElement('a')
+//         a.setAttribute('href', 'https://'+e.target.value);
+//         a.click()
+//     }
+// })
+
+// yangi event hosil qilish
+
+let inp = document.querySelector("#inp")
+
+inp.addEventListener('keydown', function(e){
+    if(!e.target.value) return;
+    if(e.target.value === 'photoprokat24' && e.key == 'Enter'){
+        let yangiE = new Event('yangi-event');
+        inp.dispatchEvent(yangiE)
     }
-
-}
-let test = document.querySelector('.el-test');
-let div = test.querySelector('div')
-let span = test.querySelector('span')
-let a = test.querySelector('a')
-let textA = test.querySelector('textarea')
-let input = test.querySelector('input')
-console.log(
-    div, span , a, textA,input
-);
-function getElementType(text){
-    let el = document.createElement
-}
-
+})
+inp.addEventListener('yangi-event', function(e){
+    console.log('Rahmat');
+})
 // el.setAttribute(attr, val)   --- el ga attribute ornatish
 // el.getAttribute(attr)    --- 
 // el.hasAttribute(attr)
